da97cd34ce669c39230f4f0d8a0c64bd2914b22a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 10:00:01 +0400

    Added Constructor

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Doctor extends Person {
@@ -12,7 +13,16 @@ public class Doctor extends Person {
     private List<String> availableSlots;
     private List<String> assignedPatients;
 
-
-
+    public Doctor(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, Double consultationFee, String departmentId, List<String> availableSlots, List<String> assignedPatients) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+        this.doctorId = doctorId;
+        this.specialization = specialization;
+        this.qualification = qualification;
+        this.experienceYears = experienceYears;
+        this.consultationFee = consultationFee;
+        this.departmentId = departmentId;
+        this.availableSlots = availableSlots;
+        this.assignedPatients = assignedPatients;
+    }
 
 }
