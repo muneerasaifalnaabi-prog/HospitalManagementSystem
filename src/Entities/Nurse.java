665cc9cb64d6d6b084321dd1278f464113881bad
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 11:39:15 +0400

    Added Constructor

--- src/Entities/Nurse.java
+++ src/Entities/Nurse.java
@@ -1,106 +1,30 @@
 package Entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
-public class Nurse extends Person{
-    private String id;
-    private String firstName;
-    private String lastName;
-    private LocalDate dateOfBirth;
-    private String gender ;
-    private String phoneNumber ;
-    private String email ;
-    private String address ;
+enum shift{
+     Morning,
+    Evening,
+    Night
 
-    public Nurse(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String id1, String firstName1, LocalDate dateOfBirth1, String lastName1, String gender1, String phoneNumber1, String email1, String address1) {
+}
+public class Nurse extends Person {
+    private String nurseId;
+    private String departmentId;
+    private shift shift;
+    private String qualification;
+    private List<String> assignedPatients;
+
+    public Nurse(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String nurseId, String departmentId, shift shift, List<String> assignedPatients, String qualification) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
-        this.id = id1;
-        this.firstName = firstName1;
-        this.dateOfBirth = dateOfBirth1;
-        this.lastName = lastName1;
-        this.gender = gender1;
-        this.phoneNumber = phoneNumber1;
-        this.email = email1;
-        this.address = address1;
+        this.nurseId = nurseId;
+        this.departmentId = departmentId;
+        this.shift = shift;
+        this.assignedPatients = assignedPatients;
+        this.qualification = qualification;
     }
 
-    @Override
-    public String getId() {
-        return id;
-    }
 
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    @Override
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    @Override
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
-
-    @Override
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String getGender() {
-        return gender;
-    }
-
-    @Override
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    @Override
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    @Override
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String getAddress() {
-        return address;
-    }
-
-    @Override
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
