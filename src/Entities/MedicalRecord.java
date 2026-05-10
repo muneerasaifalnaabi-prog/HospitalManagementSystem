7eecc9222748981fcf291e40ed2a9d6588fb5530
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 11:48:34 +0400

    Added Constructor

--- src/Entities/MedicalRecord.java
+++ src/Entities/MedicalRecord.java
@@ -12,6 +12,14 @@ public class MedicalRecord {
     private String testResults;
     private String notes;
 
-
-
+    public MedicalRecord(String recordId, String patientId, String doctorId, LocalDate visitDate, String diagnosis, String testResults, String prescription, String notes) {
+        this.recordId = recordId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.visitDate = visitDate;
+        this.diagnosis = diagnosis;
+        this.testResults = testResults;
+        this.prescription = prescription;
+        this.notes = notes;
+    }
 }
