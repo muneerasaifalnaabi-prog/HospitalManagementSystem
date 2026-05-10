990d66e8a930df425a65b2c1661b29aa05fc3d91
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 10:07:44 +0400

    Added Constructor

--- src/Entities/Nurse.java
+++ src/Entities/Nurse.java
@@ -12,4 +12,15 @@ public class Nurse extends Person{
     private String email ;
     private String address ;
 
+    public Nurse(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String id1, String firstName1, LocalDate dateOfBirth1, String lastName1, String gender1, String phoneNumber1, String email1, String address1) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+        this.id = id1;
+        this.firstName = firstName1;
+        this.dateOfBirth = dateOfBirth1;
+        this.lastName = lastName1;
+        this.gender = gender1;
+        this.phoneNumber = phoneNumber1;
+        this.email = email1;
+        this.address = address1;
+    }
 }
