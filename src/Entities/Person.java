0a2f315c8a95d8129501467530dd946b21441c0e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 09:45:53 +0400

    Added Person class With fields

---
+++ src/Entities/Person.java
@@ -0,0 +1,15 @@
+package Entities;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String id ;
+    private  String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+}
