7fb56ec9cfb8632eeb005c94e0e27895c65db3b4
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 14:30:34 +0400

    Added List<Patient> patients

--- src/Services/PatientService.java
+++ src/Services/PatientService.java
@@ -1,4 +1,20 @@
 package Services;
 
+import Entities.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class PatientService {
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
+
+    private List<Patient>patients =new ArrayList<>();
+
 }
