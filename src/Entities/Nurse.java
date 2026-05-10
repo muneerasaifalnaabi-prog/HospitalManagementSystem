37a7cb828ffb4ef98414bd7b3051190e8660fc3e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 10:07:01 +0400

    Added Fields

--- src/Entities/Nurse.java
+++ src/Entities/Nurse.java
@@ -1,4 +1,15 @@
 package Entities;
 
+import java.time.LocalDate;
+
 public class Nurse extends Person{
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender ;
+    private String phoneNumber ;
+    private String email ;
+    private String address ;
+
 }
