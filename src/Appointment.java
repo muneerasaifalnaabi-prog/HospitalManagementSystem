d980c355428cd2a0c2311bd639c3924d1958c06c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 11:56:45 +0400

    Added Constructor

--- src/Appointment.java
+++ src/Appointment.java
@@ -15,4 +15,16 @@ public class Appointment {
     private String reason;
     private String notes;
 
+    public Appointment(String appointmentId, String patientId, String doctorId, LocalDate appointmentDate, String appointmentTime, STATUSE status, String reason, String notes) {
+        this.appointmentId = appointmentId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.status = status;
+        this.reason = reason;
+        this.notes = notes;
+    }
+
+
 }
