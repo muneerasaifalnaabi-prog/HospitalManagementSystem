bdb09e63d80eae9b13074126883363f523d66e8a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 11:55:16 +0400

    Added Fields

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,3 +1,18 @@
+import java.time.LocalDate;
+enum STATUSE{
+    Scheduled,
+    Completed,
+    Cancelled,
+    Rescheduled
+}
 public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private String appointmentTime;
+    private STATUSE status;
+    private String reason;
+    private String notes;
 
 }
