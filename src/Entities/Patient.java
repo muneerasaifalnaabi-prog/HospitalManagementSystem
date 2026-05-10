77e6d7fca9cb4b99ed8aa153b431fb5bc251472e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 09:52:50 +0400

    Added Fields

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -1,5 +1,21 @@
 package Entities;
 
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Patient extends Person{
+    private String patientId ;
+    private String bloodGroup ;
+    private List<String> allergies ;
+    private String emergencyContact ;
+    private LocalDate registrationDate ;
+    private String  insuranceId ;
+    private List<String> medicalRecords;
+    private List<String> appointments;
+
+
+
+
 
 }
