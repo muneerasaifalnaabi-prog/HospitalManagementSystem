ce2c47f5ae7cc35f043cfd36f23bf5a46280eaad
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 11:48:13 +0400

    Added Fields

--- src/Entities/MedicalRecord.java
+++ src/Entities/MedicalRecord.java
@@ -1,4 +1,17 @@
 package Entities;
 
+import java.time.LocalDate;
+
 public class MedicalRecord {
+    private String recordId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate visitDate;
+    private String diagnosis;
+    private String prescription;
+    private String testResults;
+    private String notes;
+
+
+
 }
