c98ea55d0cc67cb3f3c79572799e7f6dd1f45bf9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 12:17:22 +0400

    Added PatientService class

--- src/Entities/Department.java
+++ src/Entities/Department.java
@@ -20,4 +20,70 @@ public class Department {
         this.bedCapacity = bedCapacity;
         this.availableBeds = availableBeds;
     }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getHeadDoctorId() {
+        return headDoctorId;
+    }
+
+    public void setHeadDoctorId(String headDoctorId) {
+        this.headDoctorId = headDoctorId;
+    }
+
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<Nurse> getNurses() {
+        return nurses;
+    }
+
+    public void setNurses(List<Nurse> nurses) {
+        this.nurses = nurses;
+    }
+
+    public int getBedCapacity() {
+        return bedCapacity;
+    }
+
+    public void setBedCapacity(int bedCapacity) {
+        this.bedCapacity = bedCapacity;
+    }
+
+    public int getAvailableBeds() {
+        return availableBeds;
+    }
+
+    public void setAvailableBeds(int availableBeds) {
+        this.availableBeds = availableBeds;
+    }
+    public void assignDoctor(){
+
+    }
+    public void assignNurse() {
+
+    }
+    public void updateBedAvailability() {
+
+    }
 }
+
