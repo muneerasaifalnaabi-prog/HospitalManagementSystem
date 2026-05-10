897d53a7e0f61c470d8810fce60637c4e13fd30c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-10 09:53:18 +0400

    Added Constructor

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -14,8 +14,16 @@ public class Patient extends Person{
     private List<String> medicalRecords;
     private List<String> appointments;
 
-
-
-
+    public Patient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String patientId, String bloodGroup, List<String> allergies, String emergencyContact, LocalDate registrationDate, String insuranceId, List<String> appointments, List<String> medicalRecords) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+        this.patientId = patientId;
+        this.bloodGroup = bloodGroup;
+        this.allergies = allergies;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.insuranceId = insuranceId;
+        this.appointments = appointments;
+        this.medicalRecords = medicalRecords;
+    }
 
 }
