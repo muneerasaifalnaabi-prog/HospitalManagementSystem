ba861464a3e738288c056e01c2290c00337dd276
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 14:23:38 +0400

    added menue messege for all entities

--- src/Utiles/MenuMessege.java
+++ src/Utiles/MenuMessege.java
@@ -4,34 +4,71 @@ public class MenuMessege {
     public static String MAIN_MENU_MESSEGE= """
             1. Patient Menu
             2. Doctor Menu
+            3. Nurse Menu
+            4. MedicalRecord Menu
+            5. Appointment Menu
+            6. Department Menu
            
             """;
     public static String PATIENT_MENU_MESSEGE= """
-           1.Added Patient 
-           2.edit Patient
-           3.remove Patient
-           4.Search Patient
-           4.display All Patients
-           5.exit Patient
+           1. Add Patient 
+           2. edit Patient
+           3. remove Patient
+           4. Search Patient
+           4. display All Patients
+           5. exit Patient
     """;
 
 
 public static String DOCTOR_MENU_MESSEGE= """
-           1.Added Doctor 
-           2.edit Doctor
-           3.remove Doctor
-           4.Search By Specialization
-           4.Display All Doctors
-           5.Display Available Doctors
-           5.exit Patient
+           1. Add Doctor 
+           2. edit Doctor
+           3. remove Doctor
+           4. Search By Specialization
+           4. Display All Doctors
+           5. Display Available Doctors
+           5. exit Patient
     """;
     public static String NURSE_MENU_MESSEGE= """
-           1.Added Nurse 
-           2.edit Nurse
-           3.remove Nurse
-           4.Search Nurses By Department
-           5.Search Nurses By Shift
-           6.Display All Nurses
-           7.exit Nurse
+           1. Add Nurse 
+           2. edit Nurse
+           3. remove Nurse
+           4. Search Nurses By Department
+           5. Search Nurses By Shift
+           6. Display All Nurses
+           7. exit Nurse
     """;
+    public static String DEPARTMENT_MENU_MESSEGE= """
+           1. Add Department 
+           2. edit Department
+           3. remove Department
+           4. Search Department By id
+           5. Display All Departments
+           6. Assign Doctor To Department
+           7. exit Department
+    """;
+    public static String APPOINTMENT_MENU_MESSEGE= """
+             1. Add Appointment
+             2. Edit Appointment
+             3. Delete Appointment
+             4. Display All Appointments
+             5. Search By Patient ID
+             6. Search By Doctor ID
+             7. Search By Date
+             8. Reschedule Appointment
+             9. Cancel Appointment
+             10. Exit
+            """;
+    public static String MEDICALRECORD_MENU_MESSEGE= """
+           1. Add Medical Record 
+           2. edit Medical Record
+           3. remove Medical Record
+           4. Search By Patient ID
+           5. Search By Doctor ID
+           6. Display Patient History
+           7. Display All Medical Records
+           8. exit Medical Record
+    """;
+
 }
+
