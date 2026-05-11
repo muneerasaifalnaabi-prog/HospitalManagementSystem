7506cbc33340eec68acefc39e6f176f1b5a84a73
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 15:39:18 +0400

    Added OutPatient class wit fields

--- src/Entities/OutPatient.java
+++ src/Entities/OutPatient.java
@@ -1,5 +1,13 @@
 package Entities;
 
+import java.time.LocalDate;
+
 public class OutPatient {
+    private int visitCount;
+    private LocalDate lastVisitDate;
+    private String preferredDoctorId;
+
+
+
 
 }
