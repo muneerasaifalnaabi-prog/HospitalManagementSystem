c90a01b26b47c6ef5fe391d9655b92bfd5efead2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 11:56:47 +0400

    in NurseService

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -1,6 +1,14 @@
 package Services;
 
+import Entities.Nurse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class NurseService {
+static Scanner scanner=new Scanner(System.in);
+static List<Nurse> Nurses=new ArrayList<Nurse>();
 
 
 }
