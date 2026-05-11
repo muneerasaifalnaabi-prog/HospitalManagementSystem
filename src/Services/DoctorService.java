3b9fa4df64b022c6a11b590e8ba231eb9f2c455e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 08:55:01 +0400

    added DoctorService aadDoctor

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -4,9 +4,18 @@ import Entities.Doctor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DoctorService {
     static List<Doctor> doctors= new ArrayList<Doctor>();
+    static Scanner scanner=new Scanner(System.in);
+    public static List<Doctor> searchPatientsByName(String searchName){
+
+    }
+
+    public void addDoctor(Doctor doctor){
+
+    }
 
 
     
