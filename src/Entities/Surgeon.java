1b9b7f9f3cd9a69c1514a67c7c850e21afddef4a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 14:34:25 +0400

    Added constractor

--- src/Entities/Surgeon.java
+++ src/Entities/Surgeon.java
@@ -1,5 +1,17 @@
 package Entities;
 
-public class Surgeon extends Doctor {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Surgeon extends Doctor {
+ private int surgeriesPerformed;
+ private List<String> surgeryTypes;
+ private  List<String> operationTheatreAccess;
+
+    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, List<String> operationTheatreAccess, int surgeriesPerformed, List<String> surgeryTypes) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.operationTheatreAccess = operationTheatreAccess;
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.surgeryTypes = surgeryTypes;
+    }
 }
