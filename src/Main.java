d15695402591aa3978080379c3882c4d0f36eb28
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 09:42:09 +0400

    updated in mainApp class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import Menu.Menue;
 import Services.PatientService;
+
 
 import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
     static PatientService patientService =new PatientService();
+   static Menue menue =new Menue();
     public static void main(String[] args) {
+
+        }
+
+        public void handelMenu(){
+        menue.displayMenu();
+        int choice=input.nextInt();
+        switch(choice){
+            case 1->{
+
+            }
+        }
 
         }
     }
