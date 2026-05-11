5eb590f4bb24ea4056c9877e948d0313dde90f63
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 14:49:18 +0400

    Complete Surgeon class

--- src/Entities/Surgeon.java
+++ src/Entities/Surgeon.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Surgeon extends Doctor {
  private int surgeriesPerformed;
  private List<String> surgeryTypes;
- private  List<String> operationTheatreAccess;
+ private  Boolean operationTheatreAccess;
 
-    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, List<String> operationTheatreAccess, int surgeriesPerformed, List<String> surgeryTypes) {
+    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, int surgeriesPerformed, List<String> surgeryTypes, Boolean operationTheatreAccess) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
-        this.operationTheatreAccess = operationTheatreAccess;
         this.surgeriesPerformed = surgeriesPerformed;
         this.surgeryTypes = surgeryTypes;
+        this.operationTheatreAccess = operationTheatreAccess;
     }
 
     public int getSurgeriesPerformed() {
@@ -31,11 +31,43 @@ public class Surgeon extends Doctor {
         this.surgeryTypes = surgeryTypes;
     }
 
-    public List<String> getOperationTheatreAccess() {
+    public Boolean getOperationTheatreAccess() {
         return operationTheatreAccess;
     }
 
-    public void setOperationTheatreAccess(List<String> operationTheatreAccess) {
+    public void setOperationTheatreAccess(Boolean operationTheatreAccess) {
         this.operationTheatreAccess = operationTheatreAccess;
+    }
+
+    public boolean  performSurgery(String surgeryType){
+
+        if(!surgeryTypes.contains(surgeryType)){
+            return false;
+        }
+
+        if(operationTheatreAccess){
+            return false;
+        }
+
+        surgeriesPerformed++;
+        return true;
+    }
+    public void updateSurgeryCount() {
+        surgeriesPerformed++;
+        System.out.println("Total surgeries performed: " + surgeriesPerformed);
+    }
+
+    @Override
+    public String toString() {
+        return "Surgeon{" +
+                "surgeriesPerformed=" + surgeriesPerformed +
+                ", surgeryTypes=" + surgeryTypes +
+                ", operationTheatreAccess=" + operationTheatreAccess +
+                '}';
+    }
+    public void displaySurgeonInfo(){
+        System.out.println("Surgeon info");
+        super.displayInfo();
+        System.out.println(this.toString());
     }
 }
