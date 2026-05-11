a81098e86bea8e057798e4d88f9a11bacb986e27
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 09:00:58 +0400

    added  AddDoctor method

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -9,11 +9,21 @@ import java.util.Scanner;
 public class DoctorService {
     static List<Doctor> doctors= new ArrayList<Doctor>();
     static Scanner scanner=new Scanner(System.in);
-    public static List<Doctor> searchPatientsByName(String searchName){
+    public static List<Doctor> searchPatientsByName(String searchName) {
 
+    }
+    public Doctor getDoctorById(int id){
+        for (Doctor doctor:doctors){
+
+        }
     }
 
     public void addDoctor(Doctor doctor){
+        System.out.println("===== Added Doctor =====");
+        System.out.println("Enter id of Doctor");
+        int id=scanner.nextInt();
+
+
 
     }
 
