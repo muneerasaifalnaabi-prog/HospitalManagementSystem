326fdcb003c2fa84e9ad4af96bb66291da66fb3a
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-11 15:05:27 +0400

    Added all methods

--- src/Entities/Consultant.java
+++ src/Entities/Consultant.java
@@ -1,4 +1,64 @@
 package Entities;
 
-public class Consultant {
+import java.time.LocalDate;
+import java.util.List;
+
+public class Consultant extends Doctor{
+   private List<String> consultationTypes;
+   private Boolean onlineConsultationAvailable;
+   private int consultationDuration;
+
+    public Consultant(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, Double consultationFee, List<String> availableSlots, List<String> assignedPatients, List<String> consultationTypes, int consultationDuration, Boolean onlineConsultationAvailable) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.consultationDuration = consultationDuration;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+    }
+
+    public List<String> getConsultationTypes() {
+        return consultationTypes;
+    }
+
+    public void setConsultationTypes(List<String> consultationTypes) {
+        this.consultationTypes = consultationTypes;
+    }
+
+    public Boolean getOnlineConsultationAvailable() {
+        return onlineConsultationAvailable;
+    }
+
+    public void setOnlineConsultationAvailable(Boolean onlineConsultationAvailable) {
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+    }
+
+    public int getConsultationDuration() {
+        return consultationDuration;
+    }
+
+    public void setConsultationDuration(int consultationDuration) {
+        this.consultationDuration = consultationDuration;
+    }
+    public void scheduleConsultation(String type){
+        consultationTypes.add(type);
+        System.out.println("Consultation scheduled for " + type);
+        System.out.println("Doctor :" +getFirstName());
+        System.out.println("Doctor :" +getLastName());
+        System.out.println("Duration :" +getConsultationDuration()+"minute");
+
+        if(onlineConsultationAvailable){
+            System.out.println("Doctor :" +getFirstName());
+            System.out.println("Doctor :" +getLastName());
+            System.out.println("This consultation  can be done online ");
+        }
+        else {
+            System.out.println("This consultation is only in-person.");
+        }
+
+    }
+    public void provideSecondOpinion(String diagnosis){
+        System.out.println("Doctor :" +getFirstName() + "is reviewing the diagnosis...");
+        System.out.println("Original Diagnosis :" +diagnosis);
+        System.out.println("Second Opinion Second opinion: further evaluation required / confirmed / alternative diagnosis suggested.");
+
+    }
 }
