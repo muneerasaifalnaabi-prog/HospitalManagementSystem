2f1300563af4bc084d21ee8013c73888aa8d6214
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-12 10:32:42 +0400

    Added Helper Class That i can use Multiple interface

--- src/Utils/HelperUtils.java
+++ src/Utils/HelperUtils.java
@@ -1,4 +1,14 @@
 package Utils;
 
 public class HelperUtils {
+    public Boolean isNull(Object obj){
+        return obj==null;
+    }
+    public Boolean isNull(String str ){
+        if (str.isEmpty()&& str.equals(null)){
+            return true;
+        }
+        return false;
+    }
+
 }
