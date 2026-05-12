163f486c21c97c7f73971233309a7be7c5ac652c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-12 14:07:35 +0400

    Added methods in HelperUtils

--- src/Utils/HelperUtils.java
+++ src/Utils/HelperUtils.java
@@ -1,14 +1,20 @@
 package Utils;
 
 public class HelperUtils {
-    public Boolean isNull(Object obj){
-        return obj==null;
+    public Boolean isNull(Object obj) {
+        return obj == null;
     }
-    public Boolean isNull(String str ){
-        if (str.isEmpty()|| str.equals(null)){
-            return true;
-        }
-        return false;
+
+    public Boolean isNull(String str) {
+        return str == null || str.isEmpty();
+
     }
+    public static  boolean isNotNull(Object obj){
+        return obj != null;
+    }
+    public static boolean isNotNull(String str) {
+        return  str !=null || !str.isEmpty();
+    }
+
 
 }
