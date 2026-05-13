58692cdf3b43d80e88eb654cee6b4114e8104af4
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-13 09:28:37 +0400

    overloaded mthod etStringInput(String prompt

--- src/Utils/InputHandler.java
+++ src/Utils/InputHandler.java
@@ -1,4 +1,12 @@
 package Utils;
 
+import java.util.Scanner;
+
 public class InputHandler {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String etStringInput(String prompt){
+
+    }
+
+
 }
