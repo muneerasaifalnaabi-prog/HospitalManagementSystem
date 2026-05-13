060fa41c179bda43ac24f4cbf791f48e01b5c711
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-13 09:30:14 +0400

    overloaded mthod etStringInput(String prompt

--- src/Utils/InputHandler.java
+++ src/Utils/InputHandler.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class InputHandler {
     public static Scanner scanner = new Scanner(System.in);
     public static String etStringInput(String prompt){
+        while(true){
+            System.out.print(prompt);
+            String input = scanner.nextLine();
+            if(HelperUtils.isValidString(input)){
+                return input;
+            }
+            System.out.println("Please enter a valid string");
+        }
 
     }
 
