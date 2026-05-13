dc90f3ecad88851a90f5df83a9cdbbf9f4c1fe32
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-13 20:12:25 +0400

    Added DailyAppointmentsReport method

--- src/Services/ReportService.java
+++ src/Services/ReportService.java
@@ -1,7 +1,24 @@
 package Services;
 
+import Entities.Appointment;
+import Utils.InputHandler;
+
+import java.time.LocalDate;
+
 public class ReportService {
     public void DailyAppointmentsReport() {
+        System.out.println("====Daily Appointments Report=====");
+        LocalDate localDate = InputHandler.getLocalDateInput("Enter Daily Appointment Date");
+        boolean found = false;
+       for (Appointment a :AppointmentService.appointments){
+           if (a.getAppointmentDate().equals(localDate)){
+               System.out.println("Appointment Information in Details");
+               a.displayInfo();
+               System.out.println("Summary of Appointment Information ");
+               a.displaySummary();
+               found = true;
+           }
+       }
 
     }
 }
